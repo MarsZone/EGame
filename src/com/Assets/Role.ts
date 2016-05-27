2af895018f44425aa9fa9f5cb048db710425005b
zm03322161@qq.com <zm03322161@qq.com>
2016-05-27 19:26:50 +0800

    Add sprite to role

--- src/com/Assets/Role.ts
+++ src/com/Assets/Role.ts
@@ -9,11 +9,22 @@ module Assets {
     	    super();
 		}
         animiations:Array<Assets.Animation>;
+        display:egret.Bitmap;
+        textures:Tools.Map;
         
         init(id:number, kind:string):void{
             super.init(id,kind);
             this.id=id;
             this.kind=kind;
+            var bigTexture:egret.Texture = RES.getRes("leatherarmor_png");
+            var sps:egret.SpriteSheet  = new egret.SpriteSheet(bigTexture);
+            var imgJson = RES.getRes("leatherarmor_json");
+            for(var animate_json in imgJson.animations)
+            {
+                Main.debugView.addLog(""+animate_json);
+                var animate:Assets.Animation = new Assets.RoleAnimation;
+                
+            }
         }
 	}
 }
